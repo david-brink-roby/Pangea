e7b68c5359904734983f6970e80e51b0fb48dcdd
Skyler Clagg <skylerclagg1@gmail.com>
2025-04-30 22:53:40 -0400

    Deploy Flutter web to docs/ for GitHub Pages

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -52,14 +52,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "9398dc3aa1c1ef05a77b595e79df825a
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "ba0f3816915c2e6421f3f538b9763bde",
+"flutter_bootstrap.js": "a2abfe256fb26d60e43b4120891c685b",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "b885b1769ed8cb5075aa3dd41087fc0d",
 "/": "b885b1769ed8cb5075aa3dd41087fc0d",
-"main.dart.js": "0afe5d67c96c83e4de1d43afb4bf6f11",
+"main.dart.js": "22f2b576d00597153a826c99718eebb8",
 "manifest.json": "86b6c8ee9e53441604080fc344a53eb7",
 "version.json": "88c37cb86d6ac92816be1bb8c1272fac"};
 // The application shell files that are downloaded before a service worker can
