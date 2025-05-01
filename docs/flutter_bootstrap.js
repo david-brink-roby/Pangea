55a8bd63a3d6e7db21bb075f06b7c852d8a9f492
Skyler Clagg <skylerclagg1@gmail.com>
2025-04-30 22:12:41 -0400

    Deploy Flutter web to docs/ for GitHub Pages

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "113453995"
+    serviceWorkerVersion: "2962913739"
   }
 });
